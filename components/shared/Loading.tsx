6a2afee826c30d4404e5f5654e73997753f92724
IsmailDX <ismaildxx@gmail.com>
2024-01-24 04:21:04 +0400

    Finished Splash screen yay :D

--- components/shared/Loading.tsx
+++ components/shared/Loading.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 const Loading = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-black absolute z-30">
-      <p>Loading...</p>
+    <div className="w-full h-screen flex sm:justify-end sm:items-end justify-center items-center bg-black absolute z-30">
+      <Image
+        src="/images/loadingIcon.jpg"
+        alt="loadingIcon"
+        className="object-contain p-10 animate-pulse"
+        width={280}
+        height={280}
+      />
     </div>
   );
 };
