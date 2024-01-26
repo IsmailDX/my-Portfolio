5b7bb307748691ab9311568f88ea521db482616e
IsmailDX <ismaildxx@gmail.com>
2024-01-26 23:47:02 +0400

    Testing laoding section1

--- app/myStory/ar/page.tsx
+++ app/myStory/ar/page.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Index from "@/components/sections/index";
 import { getmyStoryPage } from "@/sanity/sanity-utils";
 import SectionOne from "@/components/sections/sectionOne";
+import Loading from "@/app/loading";
+import { Suspense } from "react";
 
 const MyStory = async () => {
   const homeItems = await getmyStoryPage();
   return (
     <div className="w-full h-full">
       <Index items={homeItems} />
-      <SectionOne items={homeItems} />
+      <Suspense fallback={<Loading />}>
+        <SectionOne items={homeItems} />
+      </Suspense>
     </div>
   );
 };
