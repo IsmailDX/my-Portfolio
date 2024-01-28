070b5c479a01091593dde444af9a1c5427886bc6
IsmailDX <ismaildxx@gmail.com>
2024-01-29 00:35:22 +0400

    test

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -12,36 +12,38 @@ const Index = ({ items }: Props) => {
   const colorMode = useAppSelector((state) => state.color.value);
 
   return (
-    <div className="w-full h-[75dvh] fixed bg-black overflow-hidden">
-      {items.map(
-        (item) =>
-          item.colorMode === colorMode && (
-            <div
-              key={item._id}
-              className={`w-full h-full flex items-end ${
-                colorMode === "dark" ? "justify-start" : "justify-center"
-              }`}
-            >
-              <Image
-                src={item.image}
-                alt={item.name}
-                width={490}
-                height={490}
-                className="absolute h-[88%] w-fit object-center object-cover"
-              />
+    <section className="w-full h-[100dvh] fixed bg-black overflow-hidden">
+      <div className="w-full h-[75dvh] fixed">
+        {items.map(
+          (item) =>
+            item.colorMode === colorMode && (
+              <div
+                key={item._id}
+                className={`w-full h-full flex items-end ${
+                  colorMode === "dark" ? "justify-start" : "justify-center"
+                }`}
+              >
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={490}
+                  height={490}
+                  className="absolute h-[88%] w-fit object-center object-cover"
+                />
 
-              <video
-                src={item.video}
-                autoPlay
-                playsInline
-                muted
-                loop
-                className={`w-full h-full object-center object-cover pointer-events-none`}
-              />
-            </div>
-          )
-      )}
-    </div>
+                <video
+                  src={item.video}
+                  autoPlay
+                  playsInline
+                  muted
+                  loop
+                  className={`w-full h-full object-center object-cover pointer-events-none`}
+                />
+              </div>
+            )
+        )}
+      </div>
+    </section>
   );
 };
 
