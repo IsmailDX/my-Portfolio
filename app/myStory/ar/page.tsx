60894841395870dbffffadd493346fd4045b771a
IsmailDX <ismaildxx@gmail.com>
2024-02-01 20:48:49 +0400

    testing 14

--- app/myStory/ar/page.tsx
+++ app/myStory/ar/page.tsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import Index from '@/components/sections/index'
+import React from "react";
+import Index from "@/components/sections/index";
 import {
-    getmyMedia,
-    getmyContent,
-    getmyColorImage,
-    getmyEducation,
-} from '@/sanity/sanity-utils'
-import AllSections from '@/components/sections/allSections'
-import Loading from '@/app/loading'
-import { Suspense } from 'react'
+  getmyMedia,
+  getmyContent,
+  getmyColorImage,
+  getmyEducation,
+} from "@/sanity/sanity-utils";
+import AllSections from "@/components/sections/allSections";
+import Loading from "@/app/loading";
+import { Suspense } from "react";
 
 const MyStory = async () => {
-    const media = await getmyMedia()
-    const content = await getmyContent()
-    const colorImage = await getmyColorImage()
-    const education = await getmyEducation()
-    return (
-        <div className="w-full h-full">
-            <title>Portfolio | قصتي</title>
-            <Suspense fallback={<Loading />}>
-                <Index items={media} />
-                <AllSections
-                    items={media}
-                    content={content}
-                    colorImage={colorImage}
-                    education={education}
-                />
-            </Suspense>
-        </div>
-    )
-}
+  const media = await getmyMedia();
+  const content = await getmyContent();
+  const colorImage = await getmyColorImage();
+  const education = await getmyEducation();
+  return (
+    <div className="w-full h-[100lvh]">
+      <title>Portfolio | قصتي</title>
+      <Suspense fallback={<Loading />}>
+        <Index items={media} />
+        <AllSections
+          items={media}
+          content={content}
+          colorImage={colorImage}
+          education={education}
+        />
+      </Suspense>
+    </div>
+  );
+};
 
-export default MyStory
+export default MyStory;
