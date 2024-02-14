5a8d09d994d545b42ebbfc568e9fa522ebe464bd
IsmailDX <ismaildxx@gmail.com>
2024-02-14 22:40:17 +0400

    checking metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ import { Suspense } from "react";
 export const metadata: Metadata = {
   title: "Ismail | Portfolio",
   description: "A website about Ismail and his work",
-  icons: "/favicon.ico",
+  assets: ["/images/webIcon.png"],
+  icons: ["/images/webIcon.png"],
 };
 
 export default function RootLayout({
