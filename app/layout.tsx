97503ecc345f7681b075c1fab614e9b3678d6daf
IsmailDX <ismaildxx@gmail.com>
2024-02-14 23:32:53 +0400

    checking metadata 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,20 @@ import Loading from "./loading";
 import { Suspense } from "react";
 
 export const metadata = {
-  title: "Ismail's Portfolio",
-  description: "Checkout Ismail's amazing portfolio",
+  metadataBase: new URL("https://ismaildx.vercel.app/"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+      "ar-DE": "/de-DE",
+    },
+  },
   openGraph: {
     title: "Ismail's Portfolio",
-    description: "Checkout Ismail's amazing portfolio",
-    type: "website",
+    description: "The React Framework for the Web",
     url: "https://ismaildx.vercel.app/",
-    images: ["/images/webIcon.png"],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Ismail's Portfolio",
-    description: "Checkout Ismail's amazing portfolio",
-    images: ["/images/webIcon.png"],
+    siteName: "Ismail's Portfolio",
+    type: "website",
   },
 };
 
